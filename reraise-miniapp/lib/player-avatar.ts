abf3b9211c7a8fce4e521d9be7be9777e25ebdee
Kirill <kkyuchkin.kirill@gmail.com>
2026-03-19 00:20:06 +0300

    Аватар 9

--- reraise-miniapp/lib/player-avatar.ts
+++ reraise-miniapp/lib/player-avatar.ts
@@ -4,27 +4,12 @@ export type AvatarSource = {
   telegram_avatar_url?: string | null;
 };
 
-function isTelegramPlaceholderAvatar(url: string) {
-  return /^https:\/\/t\.me\/i\/userpic\//i.test(url);
-}
-
 export function getPlayerAvatarUrl(player: AvatarSource | null | undefined) {
   if (!player) {
     return null;
   }
 
-  if (player.custom_avatar_url) {
-    return player.custom_avatar_url;
-  }
-
-  if (
-    player.telegram_avatar_url &&
-    !isTelegramPlaceholderAvatar(player.telegram_avatar_url)
-  ) {
-    return player.telegram_avatar_url;
-  }
-
-  return null;
+  return player.custom_avatar_url ?? player.telegram_avatar_url ?? null;
 }
 
 export function getPlayerAvatarFallback(player: AvatarSource | null | undefined) {
